2f3343b8b5ece22f09bf0d6a1e99408ef365cb28
BraydonS97 <BraydonS97@gmail.com>
2024-04-11 18:49:25 -0400

    Added error logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from keys import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
+import traceback
 import schedule
 import random
 import tweepy
@@ -21,9 +22,13 @@ client = tweepy.Client(
     wait_on_rate_limit=True,
 )
 
+
+
 # Select a random image from external folder
 def select_random_image(folder):
     images = [f for f in os.listdir(folder) if f.endswith('.jpg')]
+    if not images:
+        raise FileNotFoundError("No image files found in the specified folder.")
     return os.path.join(folder, random.choice(images))
 
 # Select a random paragraph from json
@@ -37,54 +42,59 @@ def count_images_in_folder(folder):
 
 # Upload image to twitter and create tweet.
 def upload_image_and_create_tweet():
-    # Meme Folder path
-    external_folder = 'YOUR_FOLDER_PATH'
+    try:
+        # Meme Folder path
+        external_folder = 'YOUR_FOLDER_PATH'
 
-    # Check the number of images in the folder
-    num_images = count_images_in_folder(external_folder)
+        # Check the number of images in the folder
+        num_images = count_images_in_folder(external_folder)
 
-    # Warn if there are less than x imgs
-    if num_images < 24:
-        print(f"WARNING: There are {num_images} Img's left in the folder")
+        # Warn if there are less than x imgs
+        if num_images < 24:
+            print(f"WARNING: There are {num_images} Img's left in the folder")
 
-    # Select a random image from the external folder
-    image_path = select_random_image(external_folder)
+        # Select a random image from the external folder
+        image_path = select_random_image(external_folder)
 
-    # Select a random text from JSON file and then remove it from the JSON
-    with open('random_text.json', 'r+') as file:
-        paragraphs = json.load(file)
+        # Select a random text from JSON file and then remove it from the JSON
+        with open('random_text.json', 'r+') as file:
+            paragraphs = json.load(file)
 
-        # Select a random paragraph
-        selected_paragraph = random.choice(paragraphs)
+            # Select a random paragraph
+            selected_paragraph = random.choice(paragraphs)
 
-        # Remove selected Paragraph
-        paragraphs.remove(selected_paragraph)
+            # Remove selected Paragraph
+            paragraphs.remove(selected_paragraph)
 
-        # Set the file cursor at the beginning
-        file.seek(0)
+            # Set the file cursor at the beginning
+            file.seek(0)
 
-        # Write the modified data back to JSON
-        json.dump(paragraphs, file, indent=4)
+            # Write the modified data back to JSON
+            json.dump(paragraphs, file, indent=4)
 
-        # Remove any remaining content after the newly written data
-        file.truncate()
+            # Remove any remaining content after the newly written data
+            file.truncate()
 
-    # Upload image to Twitter
-    media_id = api.media_upload(filename=image_path).media_id_string
+        # Upload image to Twitter
+        media_id = api.media_upload(filename=image_path).media_id_string
 
-    # Text for the tweet if not paragraph the tweet will have no text
-    if selected_paragraph:
-        text = selected_paragraph
-    else:
-        text = ""
+        # Text for the tweet if not paragraph the tweet will have no text
+        if selected_paragraph:
+            text = selected_paragraph
+        else:
+            text = ""
 
-    # Create tweet with text and img
-    client.create_tweet(text=text, media_ids=[media_id])
-    print("Tweeted: ", selected_paragraph + " IMGs: ", image_path)
+        # Create tweet with text and img
+        client.create_tweet(text=text, media_ids=[media_id])
+        print("Tweeted: ", selected_paragraph + " IMGs: ", image_path)
 
-    # Delete the image file after posting
-    os.remove(image_path)
-    print("Paragraph's Remaining: ", str(len(paragraphs)), " Img's Remaining: ", num_images)
+        # Delete the image file after posting
+        os.remove(image_path)
+        print("Paragraph's Remaining: ", str(len(paragraphs)), " Img's Remaining: ", num_images)
+    except Exception as e:
+        # Log error
+        print("An error occured: ", e)
+        traceback.print_exc()
 
 
 posting_time = input("Enter the minute interval (00-59): ")
@@ -96,10 +106,16 @@ print("Bot started...")
 
 # Run the scheduler
 while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
+    try:
+        schedule.run_pending()
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
+    except Exception as e:
+        print("An error occured:", e)
+        traceback.print_exc()
+        continue
 
 
 
